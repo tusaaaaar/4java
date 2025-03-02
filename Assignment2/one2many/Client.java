748640bf86c1a74c4140c9f7366281f207b5e76a
tusaaaaar <158155625+tusaaaaar@users.noreply.github.com>
2025-03-02 21:25:20 +0530

    kar diya

--- Assignment2/one2many/Client.java
+++ Assignment2/one2many/Client.java
@@ -1,26 +1,32 @@
 import java.io.*;
 import java.net.*;
 
-public class Client {
+public class ClientSideThree {
     public static void main(String[] args) {
         try {
-            // Server se connect ho raha hai
+            // Server se connection banane ki koshish
             Socket socket = new Socket("localhost", 6666);
             System.out.println("Server se connection ho gaya!");
 
+            // User se input lene ke liye stream
             DataInputStream consoleIn = new DataInputStream(System.in);
+
+            // Server ko data bhejne ke liye stream
             DataOutputStream dataOut = new DataOutputStream(socket.getOutputStream());
 
             String msg = "";
 
-            // Jab tak "over" nahi likhoge, tab tak message bhej sakte ho
+            // Jab tak "over" nahi likhoge, tab tak message bheja ja sakta hai
             while (!msg.equals("over")) {
-                msg = consoleIn.readLine();
-                dataOut.writeUTF(msg);
+                System.out.print("Message likho: ");
+                msg = consoleIn.readLine(); // Console se user ka message lena
+                dataOut.writeUTF(msg); // Server ko bhejna
             }
 
+            // User ne chat band kar di, ab connection close karna hai
             System.out.println("Tumne chat band kar di!");
 
+            // Streams aur socket close karna zaroori hai
             consoleIn.close();
             dataOut.close();
             socket.close();
