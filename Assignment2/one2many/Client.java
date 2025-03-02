3ce6510b0c9efd6a66fb4c73175c899eb833f3cb
tusaaaaar <158155625+tusaaaaar@users.noreply.github.com>
2025-03-02 21:25:41 +0530

    kar diya

--- Assignment2/one2many/Client.java
+++ Assignment2/one2many/Client.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.net.*;
 
-public class ClientSideThree {
+public class Client {
     public static void main(String[] args) {
         try {
             // Server se connection banane ki koshish
