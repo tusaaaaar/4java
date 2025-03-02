d27712364c6ceba8bc1ef401951e6d62ffb099df
tusaaaaar <158155625+tusaaaaar@users.noreply.github.com>
2025-03-02 21:23:04 +0530

    kar diya

--- Assignment2/one2many/Client.java
+++ Assignment2/one2many/Client.java
@@ -1,1 +1,31 @@
-Working on it
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            // Server se connect ho raha hai
+            Socket socket = new Socket("localhost", 6666);
+            System.out.println("Server se connection ho gaya!");
+
+            DataInputStream consoleIn = new DataInputStream(System.in);
+            DataOutputStream dataOut = new DataOutputStream(socket.getOutputStream());
+
+            String msg = "";
+
+            // Jab tak "over" nahi likhoge, tab tak message bhej sakte ho
+            while (!msg.equals("over")) {
+                msg = consoleIn.readLine();
+                dataOut.writeUTF(msg);
+            }
+
+            System.out.println("Tumne chat band kar di!");
+
+            consoleIn.close();
+            dataOut.close();
+            socket.close();
+        } catch (Exception e) {
+            System.out.println("Client error: " + e);
+        }
+    }
+}
